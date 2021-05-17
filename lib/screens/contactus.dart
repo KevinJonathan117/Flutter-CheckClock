4d6a862735c167d399df763af602513864840dcc
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-05-17 23:00:34 +0700

    Added settings and contact us page

--- lib/screens/contactus.dart
+++ lib/screens/contactus.dart
@@ -11,6 +11,63 @@ class ContactUs extends StatelessWidget {
         elevation: 0,
       ),
       drawer: DrawerComponent(),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "© Check-Clock 2021",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              Text(
+                "Developed by Technopreneurship II Kelompok 25",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(height: 50),
+              Text(
+                "Phone: +6281234567890",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "LINE OA: @checkclock.id",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "IG: @checkclock.id",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 25),
+              Text(
+                "Email: info@checkclock.co.id",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:check_clock/shared/drawer.dart';
 
-class Settings extends StatelessWidget {
+class Settings extends StatefulWidget {
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  bool useBiometrics = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +18,48 @@ class Settings extends StatelessWidget {
         elevation: 0,
       ),
       drawer: DrawerComponent(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            children: [
+              Card(
+                elevation: 5,
+                child: ListTile(
+                  onTap: () {},
+                  leading: Icon(
+                    Icons.person_rounded
+                  ),
+                  title: Text(
+                    "Kevin Jonathan",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text("Administrator"),
+                  trailing: Icon(Icons.qr_code),
+                ),
+              ),
+              Card(
+                elevation: 5,
+                child: SwitchListTile(
+                  value: useBiometrics,
+                  title: Text(
+                    "Use Biometrics",
+                  ),
+                  onChanged: (bool value) {
+                    setState(() {
+                      useBiometrics
+                          ? useBiometrics = false
+                          : useBiometrics = true;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
