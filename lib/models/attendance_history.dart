4322a61340a740f87188229612e3732d338de6d4
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-05-27 21:08:41 +0700

    Added history content

---
+++ lib/models/attendance_history.dart
@@ -0,0 +1,8 @@
+class AttendanceHistory {
+  String tanggal;
+  String status;
+  String masuk;
+  String keluar;
+
+  AttendanceHistory({this.tanggal, this.status, this.masuk, this.keluar});
+}
--- lib/screens/menu/history.dart
+++ lib/screens/menu/history.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:check_clock/models/attendance_history.dart';
 
 class History extends StatelessWidget {
+  List<AttendanceHistory> attendanceHistory = [
+    AttendanceHistory(
+        tanggal: "2 Desember 2020", status: "Absen", masuk: "-", keluar: "-"),
+    AttendanceHistory(
+        tanggal: "1 Desember 2020",
+        status: "Hadir",
+        masuk: "7:30",
+        keluar: "19:00"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,6 +19,32 @@ class History extends StatelessWidget {
         title: Text('History Absensi'),
         backgroundColor: Colors.blue[800],
         elevation: 0,
+      ),
+      body: Container(
+        child: ListView.builder(
+            itemCount: attendanceHistory.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Text(
+                        attendanceHistory[index].tanggal,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    subtitle: Text(
+                        "${attendanceHistory[index].masuk} until ${attendanceHistory[index].keluar}"),
+                    leading: Icon(Icons.history),
+                    trailing: Text(attendanceHistory[index].status,),
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
